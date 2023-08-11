8221169be5bbaf9c8cb4e9ffff3f3a5b2162a5bd
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2023-08-11 18:56:44 -0300

    Adicionando linter como dependencia

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <>
-    <h1>Marques-bank</h1>
+      <h1>Marques-bank</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

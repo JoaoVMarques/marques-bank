08638fe5eda5fb1d85f26d4913f6cf47f366984f
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2023-08-11 21:50:37 -0300

    Botando espaçamento e divindindo a tela em duas partes

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,10 +1,18 @@
+import { Col, Container, Row } from 'react-bootstrap';
 import logo from '../../imgs/logo.png';
 
 function Login() {
   return (
-    <>
-      <img src={logo} alt="logo da empresa com nome Marques-Bank" />
-    </>
+    <Container className='p-5'>
+      <Row>
+        <Col>
+          <img src={logo} alt="logo da empresa com nome Marques-Bank" />
+        </Col>
+        <Col>
+          <h1>Entrar</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 

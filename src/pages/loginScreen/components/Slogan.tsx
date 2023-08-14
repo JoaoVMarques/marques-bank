635df258402adda7190ac077c168299c75ec0712
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2023-08-14 15:39:10 -0300

    Mudando estilos pela página e mudando logo

--- src/pages/loginScreen/components/Slogan.tsx
+++ src/pages/loginScreen/components/Slogan.tsx
@@ -5,14 +5,14 @@ function Slogan() {
     <Container>
       <Row>
         <Col>
-          <h4 className='text-center mb-0 text-primary'>Tecnologia
+          <h4 className='text-center mb-0 color-purple'>Tecnologia
           <span className='text-body'> e </span>
           Velocidade</h4>
         </Col>
       </Row>
       <Row>
         <Col>
-          <h4 className='text-center'>para sua vida financeira!</h4>
+          <h4 className='text-center mb-3'>para sua vida financeira!</h4>
         </Col>
       </Row>
     </Container>

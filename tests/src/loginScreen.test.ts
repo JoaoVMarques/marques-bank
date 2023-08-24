bec921db6ea066d91240ab28fa7791ef5dd1167c
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2023-08-24 04:30:38 -0300

    Instalando jest e configurando para funcionar

--- tests/src/loginScreen.test.ts
+++ tests/src/loginScreen.test.ts
@@ -1,1 +1,7 @@
-// Aqui vai ficar os testes
+import { describe, expect, test } from '@jest/globals';
+
+describe('Testando', () => {
+  test('Verificar se os testes estão funcionando', () => {
+    expect(4).toBe(4);
+  });
+});

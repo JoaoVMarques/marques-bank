e25e83f770d1cdad2588c2c9075fdcc30ccffdf5
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2023-10-17 17:00:59 -0300

    Testando se não é possivel clicar no botao de login sem aceitar os termos

--- tests/src/RegisterScreen.test.tsx
+++ tests/src/RegisterScreen.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { RenderResult, render } from '@testing-library/react';
 import { describe, expect, test, vi } from 'vitest';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event'
@@ -6,9 +6,22 @@ import React from 'react';
 import App from '../../src/App';
 import testAccount from '../mock/accountsMock';
 
-const mockLocalStorage = () => {
+function mockLocalStorage() {
   localStorage.clear();
   localStorage.setItem('accounts', JSON.stringify([testAccount]));
+}
+
+const VALID_ACCOUNT = {
+  username: 'usernameTest',
+  password: 'passwordT',
+  email: 'emailTest@email.com'
+}
+
+async function enterInRegisterPage(screen: RenderResult) {
+  const registerButton = screen.getByRole('button', {
+    name: /criar conta/i
+  })
+  await userEvent.click(registerButton)
 }
 
 describe('Login screen', () => {
@@ -33,10 +46,31 @@ describe('Login screen', () => {
 
   test('Should be able to see the "registro" in the screen', async () => {
     const screen = render(<App />);
-    const registerButton = screen.getByRole('button', {
-      name: /criar conta/i
+    await enterInRegisterPage(screen)
+    expect(screen.getByText('Registro')).toBeInTheDocument();
+  })
+
+  test('Not should be able to login without accept terms', async () => {
+    const screen = render(<App />);
+    await enterInRegisterPage(screen)
+    
+    const usernameForm = screen.getByRole('textbox', {
+      name: /nome de usuario/i
     })
-    await userEvent.click(registerButton)
+    const emailForm = screen.getByRole('textbox', {
+      name: /endereço de email/i
+    })
+    const passwordForm = screen.getByLabelText(/senha/i)
+
+    await userEvent.type(usernameForm, VALID_ACCOUNT.username);
+    await userEvent.type(emailForm, VALID_ACCOUNT.email);
+    await userEvent.type(passwordForm, VALID_ACCOUNT.password);
+
+    const loginButton = screen.getByRole('button', {
+      name: /login/i
+    })
+
+    await userEvent.click(loginButton)
     expect(screen.getByText('Registro')).toBeInTheDocument();
   })
 });

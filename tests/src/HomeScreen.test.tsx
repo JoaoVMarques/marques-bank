5724b4e671205dfc163f0dee9e658ca51e129cac
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2023-11-24 19:13:45 -0300

    testando saldo disponivel, dinheiro depositado, retirado e obtido

--- tests/src/HomeScreen.test.tsx
+++ tests/src/HomeScreen.test.tsx
@@ -1,7 +1,7 @@
 import { expect, vi } from "vitest";
 import { account, testAccountInfo } from "../mock/accountsMock";
 import App from '../../src/App';
-import { render } from "@testing-library/react";
+import { RenderResult, render } from "@testing-library/react";
 import React from "react";
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom';
@@ -32,8 +32,7 @@ describe('Home screen', () => {
     });
   });
 
-  test('Should appear username in the screen', async () => {
-    const screen = render(<App />);
+  const login = async (screen: RenderResult) => {
     const emailForm = screen.getByLabelText(/endereço de email/i);
     const passwordForm = screen.getByLabelText(/senha/i);
     const loginButton = screen.getByRole('button', {
@@ -44,7 +43,26 @@ describe('Home screen', () => {
     await userEvent.type(passwordForm, account.password)
 
     await userEvent.click(loginButton);
+  }
+
+  test('Should appear username in the screen', async () => {
+    const screen = render(<App />);
+    login(screen);
     const username = await screen.findByText(testAccountInfo.username)
     expect(username).toBeInTheDocument()
   })
+
+  test('Should appear summary with available, deposited, etc..'), async () => {
+    const screen = render(<App />);
+    login(screen);
+    const available = await screen.findAllByText(testAccountInfo.balance.available)
+    const deposited = await screen.findAllByText(testAccountInfo.balance.deposited)
+    const income = await screen.findAllByText(testAccountInfo.balance.income)
+    const withdrawn = await screen.findAllByText(testAccountInfo.balance.withdrawn)
+
+    expect(available).toBeInTheDocument()
+    expect(deposited).toBeInTheDocument()
+    expect(income).toBeInTheDocument()
+    expect(withdrawn).toBeInTheDocument()
+  }
 });
